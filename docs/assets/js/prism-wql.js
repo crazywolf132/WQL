4620ad4659c66cec77288d4fec1afa3e05ec9b74
crazywolf132 <crazywolf132@gmail.com>
2020-05-17 00:03:22 +1000

    Added a few new operators

--- docs/assets/js/prism-wql.js
+++ docs/assets/js/prism-wql.js
@@ -4,15 +4,18 @@ Prism.languages.WQL = {
 		greedy: true
 	},
 	number: /(?:\B-|\b)\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
-	boolean: /\b(?:true|false)\b/,
+	boolean: /\b(?:true|false|null)\b/,
 	// variable: /\$[a-z_]\w*/i,
 	variable: /@[a-z_]\w*/i,
-	keyword: /&[a-z_]\w*/i,
+	keyword: /\b(?:STRING|NUMBER|LIST|OBJ|BOOL|#)\b/,
 	'attr-name': {
 		pattern: /[a-z_]\w*(?=\s*(?:\((?:[^()"]|"(?:\\.|[^\\"\r\n])*")*\))?:)/i,
 		greedy: true
 	},
-	operator: /[!=:|]|\.{3}/,
-	punctuation: /[!(){}\[\]=,<>]/,
+	operator: {
+		pattern: /(^|[^.])(?:\+[+=]?|-[-=]?|!=?|<<?=?|>>?>?=?|==?|&[&=]?|and|@|#|\|[|=]?|or|is|\*=?|\/=?|%=?|\^=?|[?:~])/m,
+		lookbehind: true
+	},
+	punctuation: /[!(){}\[\]=~,<>]/,
 	constant: /\b(?!ID\b)[A-Z][A-Z_\d]*\b/
 };
